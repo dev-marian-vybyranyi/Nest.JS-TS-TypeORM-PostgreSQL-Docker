2bff71a6b15284ea049a516d6a25236a453d105e
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-29 16:33:07 +0200

    create a dunny service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { DummyService } from './dummy/dummy.service';
 
 @Injectable()
 export class AppService {
+  constructor (
+    private readonly dummyServide: DummyService
+  ) {}
   getHello(): string {
-    return 'Hello World!';
+    return `Hello World! ${this.dummyServide.work()}`;
   }
 }
