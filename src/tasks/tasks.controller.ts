a89bc5d503f176cc8556a19cfb8e941d23a311fa
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-30 08:14:31 +0200

    add endpoint to retrieve a single task by ID

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('tasks')
 export class TasksController {
     @Get()
     public findAll(): string[] {
         return ["a", "b", "c"];
+    }
+
+    @Get("/:id")
+    public findOne(@Param() params: any): string {
+        return `Task ${params.id}`;
     }
 }
