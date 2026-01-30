e79aee4439eb6c7af5df001e6c361090d3bb900b
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-30 11:08:56 +0200

    Implement dynamic message prefix retrieval using NestJS ConfigService and introduce configuration typing.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { DummyService } from './dummy/dummy.service';
 import { LoggerService } from './logger/logger.service';
+import { ConfigService } from '@nestjs/config';
+import { ConfigType } from './config/config.types';
+import { AppConfig } from './config/app.config';
 
 @Injectable()
 export class AppService {
-  constructor (
+  constructor(
     private readonly dummyServide: DummyService,
-    private readonly loggerService: LoggerService
+    private readonly loggerService: LoggerService,
+    private readonly configService: ConfigService<ConfigType>,
   ) {}
   getHello(): string {
-    return this.loggerService.log(`Hello World! ${this.dummyServide.work()}`);
+    const prefig = this.configService.get<AppConfig>('app')?.messagePrefix;
+    return this.loggerService.log(
+      `${prefig} World! ${this.dummyServide.work()}`,
+    );
   }
 }
--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,6 +1,10 @@
 import { registerAs } from "@nestjs/config";
 
-export const appConfig = registerAs("app", () => ({
+export interface AppConfig {
+    messagePrefix: string;
+}
+
+export const appConfig = registerAs("app", (): AppConfig => ({
     messagePrefix: process.env.APP_MESSAGE_PREFIX ?? "Hello",
     
 }))
