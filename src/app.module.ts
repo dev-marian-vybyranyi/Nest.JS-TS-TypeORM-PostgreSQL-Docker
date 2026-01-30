8769d99a06d0f3b579aa5d685acdb756bdaba7b9
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-30 11:16:45 +0200

    Add Joi for configuration validation and define an initial schema for `APP_MESSAGE_PREFIX`.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DummyService } from './dummy/dummy.service';
-import { MessagesFormaterService } from './messages-formater/messages-formater.service';
-import { LoggerService } from './logger/logger.service';
-import { TasksModule } from './tasks/tasks.module';
-import { ConfigModule } from '@nestjs/config';
 import { appConfig } from './config/app.config';
+import { configSchema } from './config/config.types';
+import { DummyService } from './dummy/dummy.service';
+import { LoggerService } from './logger/logger.service';
+import { MessagesFormaterService } from './messages-formater/messages-formater.service';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [appConfig],
+      validationSchema: configSchema,
+      validationOptions: {
+        allowUnknown: true,
+      },
     }),
-    TasksModule
+    TasksModule,
   ],
   controllers: [AppController],
   providers: [AppService, DummyService, MessagesFormaterService, LoggerService],
