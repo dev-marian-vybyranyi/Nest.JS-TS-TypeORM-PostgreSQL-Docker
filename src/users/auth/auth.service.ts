86b4f7c05eb9aef9aad7b8cd2cb4bd4b64c0f3ff
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-02-02 10:10:49 +0200

    Implement user login functionality with password verification and JWT generation, and refine the registration conflict message.

--- src/users/auth/auth.service.ts
+++ src/users/auth/auth.service.ts
@@ -1,14 +1,20 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+    ConflictException,
+    Injectable,
+    UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from '../create-user.dto';
-import { UserService } from '../user/user.service';
+import { PasswordService } from '../password/password.service';
 import { User } from '../user.entity';
+import { UserService } from '../user/user.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly userService: UserService,
     private readonly jwtService: JwtService,
+    private readonly passwordService: PasswordService,
   ) {}
 
   public async register(createUserDto: CreateUserDto) {
@@ -17,12 +23,26 @@ export class AuthService {
     );
 
     if (existingUser) {
-      throw new ConflictException('User already exists');
+      throw new ConflictException('Email already exists');
     }
 
     const user = await this.userService.createUser(createUserDto);
 
     return user;
+  }
+
+  public async login(email: string, password: string): Promise<string> {
+    const user = await this.userService.findOneByEmail(email);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    if (!(await this.passwordService.verify(password, user.password))) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    return this.generateToken(user);
   }
 
   private generateToken(user: User): string {
