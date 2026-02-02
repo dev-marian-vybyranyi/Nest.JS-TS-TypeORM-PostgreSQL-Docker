2c0c27a0db03ad540f294ced395848163448a419
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-02-02 08:49:39 +0200

    add validation test for CreateUserDto using class-validator.

--- src/users/create-user.dto.spec.ts
+++ src/users/create-user.dto.spec.ts
@@ -1,5 +1,13 @@
+import { validate } from 'class-validator';
+import { CreateUserDto } from './create-user.dto';
+
 describe('CreateUserDto', () => {
-  it('2+2=4', () => {
-    expect(2).toBe(2);
+  it('should validate complete valid data', async () => {
+    const dto = new CreateUserDto();
+    dto.email = 'test@test.com';
+    dto.name = 'Marian';
+    dto.password = '123456';
+    const errors = await validate(dto);
+    expect(errors.length).toBe(0);
   });
 });
