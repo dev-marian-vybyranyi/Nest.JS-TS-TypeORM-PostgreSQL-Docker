e08e5c75d28610c7ee768f3e0bad8ce146050370
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-02-02 12:55:28 +0200

    add initial e2e test file for tasks module, including user registration and task creation setup.

---
+++ test/tasks.e2e-spec.ts
@@ -0,0 +1,51 @@
+import request from 'supertest';
+import { AppModule } from './../src/app.module';
+import { TaskStatus } from './../src/tasks/tasks.model';
+import { TestSetup } from './utils/test-setup';
+
+describe('Tasks (e2e)', () => {
+  let testSetup: TestSetup;
+  let authToken: string;
+  let taskId: string;
+
+  const testUser = {
+    email: 'test@example.com',
+    password: 'Password123!',
+    name: 'Test User',
+  };
+
+  beforeEach(async () => {
+    testSetup = await TestSetup.create(AppModule);
+
+    await request(testSetup.app.getHttpServer())
+      .post('/auth/register')
+      .send(testUser)
+      .expect(201);
+
+    const loginResponse = await request(testSetup.app.getHttpServer())
+      .post('/auth/login')
+      .send(testUser)
+      .expect(201);
+
+    authToken = loginResponse.body.accessToken;
+
+    const response = await request(testSetup.app.getHttpServer())
+      .post('/tasks')
+      .set('Authorization', `Bearer ${authToken}`)
+      .send({
+        title: 'Test Task',
+        description: 'Test Desc',
+        status: TaskStatus.OPEN,
+        labels: [{ name: 'test' }],
+      });
+    taskId = response.body.id;
+  });
+
+  afterEach(async () => {
+    await testSetup.cleanup();
+  });
+
+  afterAll(async () => {
+    await testSetup.teardown();
+  });
+});
