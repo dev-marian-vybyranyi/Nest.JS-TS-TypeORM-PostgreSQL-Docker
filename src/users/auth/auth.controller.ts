541bd55ee64d469110bc7d85ad77d5042fb6d9c0
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-02-02 11:04:33 +0200

    Implement user entity serialization using `ClassSerializerInterceptor` and refactor user registration logic.

--- src/users/auth/auth.controller.ts
+++ src/users/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Post,
+  SerializeOptions,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CreateUserDto } from '../create-user.dto';
 import { LoginDto } from '../login.dto';
 import { LoginResponse } from '../login.response';
@@ -6,12 +13,15 @@ import { User } from '../user.entity';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
+@UseInterceptors(ClassSerializerInterceptor)
+@SerializeOptions({ strategy: 'excludeAll' })
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
   async register(@Body() createUserDto: CreateUserDto): Promise<User> {
-    return await this.authService.register(createUserDto);
+    const user = await this.authService.register(createUserDto);
+    return user;
   }
 
   @Post('login')
