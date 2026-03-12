410baedb054440ef989a1e1061914a83920cf8a8
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-03-12 16:15:46 +0200

    add Sentry integration and update application port environment variable in workflows and `main.ts`.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as Sentry from '@sentry/node';
 
 async function bootstrap() {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    environment: process.env.APP_MESSAGE_PREFIX,
+  });
+
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
-    })
-  )
-  await app.listen(process.env.PORT ?? 3000);
+    }),
+  );
+
+  await app.listen(process.env.APP_PORT ?? 3000);
 }
 bootstrap();
